3665920e3a96413df8ed011776cffcd10f0a3064
Robb Shecter <robb@public.law>
2020-09-03 21:59:05 -0600

    refactor

--- spec/non_empty_array_spec.rb
+++ spec/non_empty_array_spec.rb
@@ -4,37 +4,30 @@
 require 'spec_helper'
 
 RSpec.describe NonEmptyArray do
+  let(:has_one) { NonEmptyArray.new('x') }
+  let(:has_three) { NonEmptyArray.new('x', %w[y z]) }
+
   it 'handles length of 1' do
-    has_one = NonEmptyArray.new('x')
     expect(has_one.count).to eq(1)
   end
 
-  it 'handles length of 2' do
-    has_one = NonEmptyArray.new('x', ['y'])
-    expect(has_one.count).to eq(2)
+  it 'handles length of 3' do
+    expect(has_three.count).to eq(3)
   end
 
   it 'does last with length 1' do
-    has_one = NonEmptyArray.new('x')
-
     expect(has_one.last).to eq('x')
   end
 
   it 'does last with length 3' do
-    has_three = NonEmptyArray.new('x', %w[y z])
-
     expect(has_three.last).to eq('z')
   end
 
   it 'does all_but_last with length 1' do
-    has_one = NonEmptyArray.new('x')
-
     expect(has_one.all_but_last).to eq([])
   end
 
   it 'does all_but_last with length 3' do
-    has_three = NonEmptyArray.new('x', %w[y z])
-
     expect(has_three.all_but_last).to eq(%w[x y])
   end
 end
